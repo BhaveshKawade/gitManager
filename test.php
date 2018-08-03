b45707c97a50a51f5cfe26c8994bcb557525d828
bhavesh <bhavesh.kawade@gmail.com>
2018-08-03 16:12:48 +0530

    added new class and test files

---
+++ test.php
@@ -0,0 +1,41 @@
+<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+ 
+
+
+<script type="text/javascript">
+	function fun(){
+
+$.ajax({
+    type : 'GET',
+    url : 'http://www.google.com',
+    dataType: 'html',
+    timeout: 10000,
+    beforeSend: function(){
+        $('.my-box').html('<div class="progress"><div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div></div>');
+        $('.progress-bar').animate({width: "30%"}, 100);
+    },
+    success: function(data){  
+        if(data == 'Unauthorized.'){
+            location.href = 'logout';
+        }else{
+            $('.progress-bar').animate({width: "100%"}, 100);
+            setTimeout(function(){
+                $('.progress-bar').css({width: "100%"});
+                setTimeout(function(){
+                    $('.my-box').html(data);
+                }, 100);
+            }, 500);
+        }
+    },
+    error: function(request, status, err) {
+    	console.log(status);
+        alert((status == "timeout") ? "Timeout" : "error: " + request + status + err);
+    }
+})
+};
+	fun();
+
+</script>
